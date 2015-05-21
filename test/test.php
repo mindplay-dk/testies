d0869326d95a24f198ecd34e47d9f9df4d6b8ca7
Rasmus Schultz <rasmus@mindplay.dk>
2015-05-21 19:10:55 +0200

    tweaks

--- test/test.php
+++ test/test.php
@@ -8,17 +8,17 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 configure()->enableVerboseOutput();
 #configure()->enableCodeCoverage(__DIR__ . '/build/clover.xml', dirname(__DIR__) . '/src');
 
+$server = new TestServer(__DIR__, 8088);
+
 test(
     'Can run a local test-server',
     function () {
-        $server = new TestServer(__DIR__, 8088);
-
         $client = new Client();
 
         $response = $client->get('http://127.0.0.1:8088/server.php');
 
-        eq($response->getStatusCode(), 200);
-        ok($response->getBody() == 'it works!');
+        eq($response->getStatusCode(), 200, 'it should return a 200 status code');
+        ok($response->getBody() == 'it works!', 'it should return the script output');
     }
 );
 
